35d4fc2a9a5524ca123301ec96167c537b244c8b
fs203 <fs2031@icloud.com>
2025-04-17 17:35:12 +0900

    finished : pass_parameter

--- Chapter2/main.py
+++ Chapter2/main.py
@@ -4,5 +4,10 @@ app = FastAPI()
 
 
 @app.get("/countries/{country_name}")
-async def country(country_name: int):
+async def country(country_name: str):
     return {"country_name": country_name}
+
+
+@app.get("/countries/japan")
+async def japan():
+    return {"message": "This is Japan"}
