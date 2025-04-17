7946b6c5a3cd62e36faa75950880a0b9dcdc5291
fs203 <fs2031@icloud.com>
2025-04-17 18:43:34 +0900

    finished : not necessary

--- Chapter2/main.py
+++ Chapter2/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/countries/{country_name}")
-async def country(country_name: str):
-    return {"country_name": country_name}
-
-
-@app.get("/countries/japan")
-async def japan():
-    return {"message": "This is Japan"}
+@app.get("/countries/")
+async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
+    return {"country_name": country_name, "country_no": country_no}
