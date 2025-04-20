f2852d2af456e228fb86374904954728b5210299
fs203 <fs2031@icloud.com>
2025-04-20 13:03:51 +0900

    deploy_test : render

--- deploy_render/sample.py
+++ deploy_render/sample.py
@@ -3,7 +3,7 @@ import json
 
 
 def main():
-    url = "https://udemy-fastapi-1hav.onrender.com/"
+    url = "http://127.0.0.1:8000"
     data = {"x": 1.2, "y": 3.0}
     res = requests.post(url, json.dumps(data))
     print(res.json())
