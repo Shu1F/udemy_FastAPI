4eb1c4676950e60cf88654d1b7a3ddd8acbd1d0a
fs203 <fs2031@icloud.com>
2025-04-20 12:44:22 +0900

    deploy_test : render

--- Chapter2/main.py
+++ Chapter2/main.py
@@ -1,28 +1,28 @@
-from fastapi import FastAPI
-from typing import Optional, List
-from pydantic import BaseModel, Field
+# from fastapi import FastAPI
+# from typing import Optional, List
+# from pydantic import BaseModel, Field
 
 
-class ShopInfo(BaseModel):
-    name: str
-    location: str
+# class ShopInfo(BaseModel):
+#     name: str
+#     location: str
 
 
-class Item(BaseModel):
-    name: str = Field(min_length=4, max_length=12)
-    description: Optional[str] = None
-    price: int
-    tax: Optional[float] = None
+# class Item(BaseModel):
+#     name: str = Field(min_length=4, max_length=12)
+#     description: Optional[str] = None
+#     price: int
+#     tax: Optional[float] = None
 
 
-class Data(BaseModel):
-    shop_info: Optional[ShopInfo]
-    items: List[Item]
+# class Data(BaseModel):
+#     shop_info: Optional[ShopInfo]
+#     items: List[Item]
 
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.post("/")
-async def index(data: Data):
-    return {"data": data}
+# @app.post("/")
+# async def index(data: Data):
+#     return {"data": data}
--- chapter1/type_hints.py
+++ chapter1/type_hints.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+# from typing import List, Dict
 
-sample_list: List[int] = [1, 2, 3, 4]
-sample_dict: Dict[str, str] = {"username": "abcd"}
+# sample_list: List[int] = [1, 2, 3, 4]
+# sample_dict: Dict[str, str] = {"username": "abcd"}
