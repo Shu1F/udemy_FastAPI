503a9d793d32e72207adaa053ebe63f5df148831
fs203 <fs2031@icloud.com>
2025-04-20 14:31:58 +0900

    finished : streamlit

--- 04_streamlit/app.py
+++ 04_streamlit/app.py
@@ -1,11 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("sample app")
-st.markdown("# sample app")
+number = st.sidebar.slider("Pick a num", 0, 100, 40)
 
-st.code(
-    """
-        import numpy as np
-        a = 123
-    """
-)
+st.sidebar.write(f"number: {number}")
+
+left_col, right_col = st.columns(2)
+left_col.slider("Pick a Num in Left", 0, 100)
+right_col.write("Right Column")
